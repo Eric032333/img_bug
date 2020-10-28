41e2b0725944f25e2af84ebf5abf67cacc01a3f4
eric <yellowtry123@gmail.com>
2020-10-29 07:29:36 +0800

    img_bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,329 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="port container">
+    <div class="row mt-4">
+
+      <div class="left-group col-lg-5"> 
+
+        <div class="card mb-3 col-lg-12">
+          <div class="row letf-gutters">
+            <div class="left-pic col-md-4">
+              <img src="https://picsum.photos/200" class="card-img" alt="..." style="">
+            </div>
+            <div class="left-words col-md-8">
+              <div class="card-body">
+                <p class="card-title" style="font-size: 1.2rem;">疫苗培養方式</p>
+                <div class='d-flex'>
+                  <p class="sort" style='color:#F2B33D;'>護理</p>
+                  <p class="by-who ml-3" style="color:#6CCDD3;">by Candy</p>
+                </div>
+                <p class="card-text">疫苗培養方式有雞胚胎蛋培養疫苗及細胞培養疫苗兩種，流感疫苗的疫苗株選擇，均依照世界衛生組織建議</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card mb-3 col-lg-12">
+          <div class="row letf-gutters">
+            <div class="left-pic col-md-4">
+              <img src="https://picsum.photos/200" class="card-img" alt="..." style="">
+            </div>
+            <div class="left-words col-md-8">
+              <div class="card-body">
+                <p class="card-title" style="font-size: 1.2rem;">疫苗培養方式</p>
+                <div class='d-flex'>
+                  <p class="sort" style='color:#F2B33D;'>護理</p>
+                  <p class="by-who ml-3" style="color:#6CCDD3;">by Candy</p>
+                </div>
+                <p class="card-text">疫苗培養方式有雞胚胎蛋培養疫苗及細胞培養疫苗兩種，流感疫苗的疫苗株選擇，均依照世界衛生組織建議</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="card mb-3 col-lg-12">
+          <div class="row letf-gutters">
+            <div class="left-pic col-md-4">
+              <img src="https://picsum.photos/200" class="card-img" alt="..." style="">
+            </div>
+            <div class="left-words col-md-8">
+              <div class="card-body">
+                <p class="card-title" style="font-size: 1.2rem;">疫苗培養方式</p>
+                <div class='d-flex'>
+                  <p class="sort" style='color:#F2B33D;'>護理</p>
+                  <p class="by-who ml-3" style="color:#6CCDD3;">by Candy</p>
+                </div>
+                <p class="card-text">疫苗培養方式有雞胚胎蛋培養疫苗及細胞培養疫苗兩種，流感疫苗的疫苗株選擇，均依照世界衛生組織建議</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div> 
+      
+      <div class="right-group col-lg-7"> 
+        <div class="col-md-12 col-lg-12 col-md-10 mb-3">
+          <div class="right card">
+            <div class="card-body d-flex">
+              <div class="media col-sm-9 col-md-8 col-lg-7">
+                <img style="width:50px; height:50px;" src="https://picsum.photos/200" class="align-self-start mr-3 mt-4" alt="...">
+                <div class="media-body">
+                  <p class="date-txt">29,6月2020</p>  
+                  <h5 class="title-line2 mt-0">親子之間的戰爭後宮甄嬛傳</h5>
+                  <p class="sort">未分類</p>
+                  <p class="txt-line2">疫苗培養方式有雞胚胎蛋培養疫苗及細胞培養疫苗兩種，流感疫苗的疫苗株選擇，均依照世界衛生組織建議，具有全球一致性且抗原性相同，其產生的保護效果是一樣的。</p>  
+                  <p class="by-who">by Josh</p>
+                </div>
+              </div>
+              <div class="pic col-sm-3 col-md-4 col-lg-5">
+                  <img src="https://picsum.photos/160" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-12 col-lg-12 col-md-10 mb-3">
+          <div class="right card">
+            <div class="card-body d-flex">
+              <div class="media col-sm-9 col-md-8 col-lg-7">
+                <img style="width:50px; height:50px;" src="https://picsum.photos/200" class="align-self-start mr-3 mt-4" alt="...">
+                <div class="media-body">
+                  <p class="date-txt">29,6月2020</p>  
+                  <h5 class="title-line2 mt-0">親子之間的戰爭後宮甄嬛傳</h5>
+                  <p class="sort">未分類</p>
+                  <p class="txt-line2">疫苗培養方式有雞胚胎蛋培養疫苗及細胞培養疫苗兩種，流感疫苗的疫苗株選擇，均依照世界衛生組織建議，具有全球一致性且抗原性相同，其產生的保護效果是一樣的。</p>  
+                  <p class="by-who">by Josh</p>
+                </div>
+              </div>
+              <div class="pic col-sm-3 col-md-4 col-lg-5">
+                  <img src="https://picsum.photos/160" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>  
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+
+  div{
+    border-style: none;
+  }
+  .card .card-body {
+    padding: 1rem;
+    align-items: center;
+  }
+
+  .card .card-body .pic img{
+    width: 100%;
+  }
+
+  .card .card-body .pic {
+    width: 14rem;
+    display: flex;
+    align-items: center;
+    padding: 0 ;
+  }
+
+
+// ====================================
+  
+  .port.container{
+    margin-bottom: 0rem;
+    .left-group{
+      .card{
+        border: 2px solid #F9F4ED; 
+        padding: 0 0 0  1rem;
+        .letf-gutters{
+          align-items: center;
+          .left-pic{
+            padding: 0 0 0 1rem;
+          }
+          .left-words{
+            padding:0;
+            .card-text {
+              padding:0 1rem 0 0 ;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              display: -webkit-box;
+              -webkit-box-orient: vertical;
+              -webkit-line-clamp: 2;
+            }
+          }
+          .left-pic{
+            img{
+              widows: 100%;
+            }
+          }
+        }
+      }
+    }
+    .right.card{
+      border: 2px solid #F9F4ED; 
+      .card-body{
+        padding:0 0 0 1rem;
+      }
+    }
+  }
+
+  .media {
+    padding: 1rem 1rem 0 0;
+    .media-body {
+      .date-txt {
+        font-size: 16px;
+        color: #B5B5B5;
+        margin-bottom: .5rem; 
+      }
+      .sort{
+        color: #F2B33D;
+        margin-bottom: .5rem; 
+        font-size: 1rem;
+      }
+      .by-who{
+        color: #6CCDD3;
+        display: flex;
+        justify-content: flex-end;
+        padding-right: 4px;
+        margin-top: .5rem;
+        font-size: 1rem;
+      }
+      .txt-line2 {
+        font-size: 1rem;
+        min-height: 50px;
+        margin-bottom: 0;
+        line-height: 1.6;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 3;
+      }
+      .title-line2 {
+        font-size: 1.2rem; 
+      }
+    }
+  }
+
+  .media img{
+    border-radius: 100%;
+  }
+
+
+
+// ====================================
+
+
+@media (max-width: 768px) {
+      
+    .port.container{
+      padding-left: 0;
+      padding-right: 0;
+      .left-group{
+        display: none;
+      }
+      .right.card{
+        .card-body{
+          
+        }
+      }
+    } 
+
+    .media {
+      padding: 0 1rem 0 0;
+      .media-body {
+        .date-txt {
+          font-size: 14px;
+          color: #B5B5B5;
+          margin-bottom: .5rem; 
+        }
+        .sort{
+          color: #F2B33D;
+          margin-bottom: .5rem; 
+        }
+        .by-who{
+          color: #6CCDD3;
+          display: flex;
+          justify-content: flex-end;
+          padding-right: 4px;
+          margin-top: .5rem;
+        }
+        .txt-line2 {
+          -webkit-line-clamp: 2;
+          font-size: 1.2rem;
+        }
+        .title-line2 {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 1;
+          font-size: 1.4rem;
+        }
+      }
+    }
+  }
+    
+
+// ====================================
+  @media (max-width: 480px) {
+      
+    .port.container{
+      padding-left: 0;
+      padding-right: 0;
+    } 
+
+    .media {
+      padding: 0 1rem 0 0;
+      .media-body {
+        .date-txt {
+          font-size: 14px;
+          color: #B5B5B5;
+          margin-bottom: .5rem; 
+        }
+        .sort{
+          color: #F2B33D;
+          margin-bottom: .5rem; 
+          font-size: 1rem;
+        }
+        .by-who{
+          p{
+            margin-bottom: 0;
+          }
+          color: #6CCDD3;
+          display: flex;
+          justify-content: flex-end;
+          padding-right: 4px;
+          margin-top: .5rem;
+          font-size: 1rem;
+        }
+        .txt-line2 {
+          min-height: 50px;
+          margin-bottom: 0;
+          line-height: 1.6;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+          font-size: 1rem;
+        }
+        .title-line2 {
+          font-size: 1rem; 
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 1;
+        }
+      }
+    }
+
+
+  }
 </style>
